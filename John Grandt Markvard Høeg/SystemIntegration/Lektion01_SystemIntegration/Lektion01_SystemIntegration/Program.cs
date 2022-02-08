b7e43e7816f9631722e61d6f480926e424d77c34
John Grandt Markvard Høeg <saphyron@hotmail.com>
2022-02-08 10:11:03 +0100

    Lektion01_MSMQ_Tutorial

---
+++ John Grandt Markvard Høeg/SystemIntegration/Lektion01_SystemIntegration/Lektion01_SystemIntegration/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+
+namespace Lektion01_SystemIntegration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MessageQueue messageQueue = null;
+            string description = "This is a test queue.";
+            string message = "This is a test message.";
+            string path = @".\Private$\IDG";
+            try {
+                if (MessageQueue.Exists(path)) {
+                    messageQueue = new MessageQueue(path);
+                    messageQueue.Label = description;
+                }
+                else {
+                    MessageQueue.Create(path);
+                    messageQueue = new MessageQueue(path);
+                    messageQueue.Label = description;
+                }
+                messageQueue.Send(message);
+            } catch {
+                throw;
+            }
+            finally {
+                messageQueue.Dispose();
+            }
+        }
+    }
+}
